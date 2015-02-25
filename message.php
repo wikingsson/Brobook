078903ba364d16a31e83dfbefccf11e6640a9205
Robin <wikingsson@live.se>
2015-02-25 13:16:31 +0100

    added message

---
+++ message.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Brobook</title>
+    <meta name="description" content="Da shit">
+    <meta name="author" content="Brobook">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href='css/maincss.css' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+  <nav role="navigation" class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="#" class="navbar-brand">BroBook</a>
+        </div>
+        <!-- Collection of nav links and other content for toggling -->
+        <div id="navbarCollapse" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="index.html">Home</a></li>
+                <li><a href="profile.html">Profile</a></li>
+                <li><a href="#">Messages</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Welcome, User <b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#"> Preferences</a></li>
+                    <li><a href="#"> Contact Support</a></li>
+                    <li class="#"></li>
+                    <li><a href="#"> Logout</a></li>
+                   </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+  <div class="row">
+
+    </div> <!-- End Row -->
+    </div> <!-- container -->
+
+      <script src="js/jquery.min.js" type="text/javascript"></script>
+      <script src="js/bootstrap.js"></script>
+      <script src="js/brobook.js"></script>
+</body>
+</html>
