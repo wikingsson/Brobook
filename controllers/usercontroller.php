41ec381261a9b886d9af666a44b8e74cebe1caea
Mikael Bang <mackeb1@gmail.com>
2015-03-04 16:37:24 +0100

    controllers
    Fixat mer med controllers och lagt till .txt med frågor till wesam

--- controllers/usercontroller.php
+++ controllers/usercontroller.php
@@ -19,7 +19,6 @@ Class Usercontroller{
 
     public function loginUser(){
 
-
         $db = new PDO("mysql:host=localhost;dbname=BroBook;charset=utf8", "root", "root");
 
         if(isset($_POST["login_submit"])){
@@ -48,6 +47,17 @@ Class Usercontroller{
 
     public function updateUser(){
         //Change PP and other stuff
+        $db = new PDO("mysql:host=localhost;dbname=BroBook;charset=utf8", "root", "root");
+
+        $updateUserStm = $db->prepare("UPDATE users SET firstname = :firstname, lastname = :lastname, profile_img = :p_img WHERE user_id = :user_id");
+        $updateUserStm->bindParam(":firstname", $_POST["first_name"]);
+        $updateUserStm->bindParam(":lastname", $_POST["last_name"]);
+        $updateUserStm->bindParam(":p_img", $_POST["profile_img"]);
+        $updateUserStm->bindParam(":user_id", $_POST["user_id"]);
+
+        $updateUserStm->execute();
+
+        //Send back to profile if everything went ok.
     }
 
 
@@ -59,6 +69,13 @@ Class Usercontroller{
     }
 
     public function deleteUser(){
-        //delete account
+        $db = new PDO("mysql:host=localhost;dbname=BroBook;charset=utf8", "root", "root");
+
+        $userDeleteStm = $db->prepare("DELETE FROM users WHERE user_id = :user_id");
+        $userDeleteStm->bindParam("user_id", $_POST["user_id"]);
+        $userDeleteStm->execute();
+
+        //Send to logout action.
+
     }
 }
