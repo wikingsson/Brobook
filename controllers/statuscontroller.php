d9cec233f05da429f737f028abfb20ac1901c810
rillvar <richard.bang@live.se>
2015-03-19 22:27:00 +0100

    klocka

--- controllers/statuscontroller.php
+++ controllers/statuscontroller.php
@@ -44,7 +44,7 @@ Class Statuscontroller{
 
         $db = new PDO("mysql:host=localhost;dbname=BroBook;charset=utf8", "root", "root");
         session_start();
-        $showStatusStm = $db->prepare("SELECT * FROM status_updates SU, users U, friends F WHERE CASE WHEN F.friend_one = :curr_user THEN F.friend_two = U.user_id WHEN F.friend_two = :curr_user THEN F.friend_one = U.user_id END AND (U.user_id = SU.user_id) AND (F.friend_one = :curr_user OR F.friend_two = :curr_user) AND f.status = 1");
+        $showStatusStm = $db->prepare("SELECT * FROM status_updates SU, users U, friends F WHERE CASE WHEN F.friend_one = :curr_user THEN F.friend_two = U.user_id WHEN F.friend_two = :curr_user THEN F.friend_one = U.user_id END AND (U.user_id = SU.user_id) AND (F.friend_one = :curr_user OR F.friend_two = :curr_user) AND f.status = 1 ORDER BY SU.status_update_id DESC");
         $showStatusStm->bindParam(":curr_user", $_SESSION["userId"]);
         $showStatusStm->execute();
 
