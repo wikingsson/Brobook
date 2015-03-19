da1c37b76169ac01a8250d46b3d2cf77f455b8fa
rillvar <richard.bang@live.se>
2015-03-19 23:10:02 +0100

    lolololo

--- views/Friends.php
+++ views/Friends.php
@@ -3,67 +3,7 @@
 
 <div class="container">
 <div class="row">
-  <div class="col-xs-12 col-md-6">
-  <div class="panel panel-default">
-           <div class="panel-heading"><h4 class="fheader">Friend requests</h4></div>
-        <div class="panel-body">
-            <?php
-            if($showFriendRequestStm->execute()){
-            while($updateRow = $showFriendRequestStm->fetch()){
-            if($updateRow["profile_img"] == null){
-                $picture = "http://www.giacomazzi.org/ArchivioImmagini/2014/ANONYMOUS_Mask_of_Guy_Fawkes.jpg";
-            }
-            else{
-                $picture = $updateRow["profile_img"];
-            }
-            ?>
-          <div class="pull-left well">
-             <center>
-              <form method="post" action="../friend/acceptFriend">
-              <a href=""><img src="<?php echo($picture)?>" name="" width="140" height="140" class="img-circle"></a>
-              <h4><?php echo($updateRow["firstname"] . " " . $updateRow["lastname"])?></h4>
-                  <input type="hidden" name="hid_user_id" value="<?php echo($updateRow["user_id"])?>" />
-              <button type="submit" name="accept_friend"  class="btn btn-default"><span>Accept Friend</span></button>
-              </form>
-              </center>
-          </div>
-            <?php
-            }
-            }
-            ?>
-
-        </div><!-- panel -->
-      </div><!-- body panel -->
-  </div><!-- col -->
-  <div class="col-xs-12 col-md-6">
-  <div class="panel panel-default">
-           <div class="panel-heading"><h4 class="fheader">Friends</h4></div>	
-        <div class="panel-body">
-            <?php
-            if($showFriendsStm->execute()){
-            while($updateRow = $showFriendsStm->fetch()){
-            if($updateRow["profile_img"] == null){
-                $picture = "http://www.giacomazzi.org/ArchivioImmagini/2014/ANONYMOUS_Mask_of_Guy_Fawkes.jpg";
-            }
-            else{
-                $picture = $updateRow["profile_img"];
-            }
-            ?>
-          <div class="pull-left well">
-             <center>
-              <a href=""><img src="<?php echo($picture)?>" name="" width="140" height="140" class="img-circle"></a>
-              <h4><?php echo($updateRow["firstname"] . " " . $updateRow["lastname"])?></h4>
-              <button type="submit" name="remove_friend" value="" class="btn btn-default"><span>Remove Friend</span></button>
-              </center>
-          </div>
-            <?php
-            }
-            }
-            ?>
-
-        </div><!-- panel -->
-      </div><!-- body panel -->
-  </div><!-- col -->
+<!-- col -->
     <div class="col-xs-12  col-md-6">
   <div class="panel panel-default">
            <div class="panel-heading"><h4 class="fheader">People you may know</h4></div>
@@ -95,6 +35,67 @@
         </div><!-- panel -->
       </div>  <!-- body panel -->
     </div><!-- col -->
+    <div class="col-xs-12 col-md-6">
+        <div class="panel panel-default">
+            <div class="panel-heading"><h4 class="fheader">Friends</h4></div>
+            <div class="panel-body">
+                <?php
+                if($showFriendsStm->execute()){
+                    while($updateRow = $showFriendsStm->fetch()){
+                        if($updateRow["profile_img"] == null){
+                            $picture = "http://www.giacomazzi.org/ArchivioImmagini/2014/ANONYMOUS_Mask_of_Guy_Fawkes.jpg";
+                        }
+                        else{
+                            $picture = $updateRow["profile_img"];
+                        }
+                        ?>
+                        <div class="pull-left well">
+                            <center>
+                                <a href=""><img src="<?php echo($picture)?>" name="" width="140" height="140" class="img-circle"></a>
+                                <h4><?php echo($updateRow["firstname"] . " " . $updateRow["lastname"])?></h4>
+                                <button type="submit" name="remove_friend" value="" class="btn btn-default"><span>Remove Friend</span></button>
+                            </center>
+                        </div>
+                    <?php
+                    }
+                }
+                ?>
+
+            </div><!-- panel -->
+        </div><!-- body panel -->
+    </div><!-- col -->
+    <div class="col-xs-12 col-md-6">
+        <div class="panel panel-default">
+            <div class="panel-heading"><h4 class="fheader">Friend requests</h4></div>
+            <div class="panel-body">
+                <?php
+                if($showFriendRequestStm->execute()){
+                    while($updateRow = $showFriendRequestStm->fetch()){
+                        if($updateRow["profile_img"] == null){
+                            $picture = "http://www.giacomazzi.org/ArchivioImmagini/2014/ANONYMOUS_Mask_of_Guy_Fawkes.jpg";
+                        }
+                        else{
+                            $picture = $updateRow["profile_img"];
+                        }
+                        ?>
+                        <div class="pull-left well">
+                            <center>
+                                <form method="post" action="../friend/acceptFriend">
+                                    <a href=""><img src="<?php echo($picture)?>" name="" width="140" height="140" class="img-circle"></a>
+                                    <h4><?php echo($updateRow["firstname"] . " " . $updateRow["lastname"])?></h4>
+                                    <input type="hidden" name="hid_user_id" value="<?php echo($updateRow["user_id"])?>" />
+                                    <button type="submit" name="accept_friend"  class="btn btn-default"><span>Accept Friend</span></button>
+                                </form>
+                            </center>
+                        </div>
+                    <?php
+                    }
+                }
+                ?>
+
+            </div><!-- panel -->
+        </div><!-- body panel -->
+    </div>
 </div><!-- row -->
 </div> <!-- container -->
 
