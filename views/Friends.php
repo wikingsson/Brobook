1c0c8bff889120a9b3482ca8dfb78e03de5b349d
rillvar <richard.bang@live.se>
2015-03-20 09:34:24 +0100

    sd

--- views/Friends.php
+++ views/Friends.php
@@ -6,7 +6,7 @@
 <!-- col -->
     <div class="col-xs-12  col-md-6">
   <div class="panel panel-default">
-           <div class="panel-heading"><h4 class="fheader">People you may know</h4></div>
+           <div class="panel-heading"><h4 class="fheader">All Users On BroBook</h4></div>
         <div class="panel-body">
             <?php
             if($showAllUsersStm->execute()){
@@ -18,16 +18,17 @@
                 $picture = $userRow["profile_img"];
             }
             ?>
-          <form method="post" action="../friend/addFriend">
               <div class="pull-left well">
-                 <center>
-                  <a href=""><img src="<?php echo($picture)?>" name="" width="140" height="140" class="img-circle"></a>
+                  <center>
+                      <form method="post" action="../friend/addFriend">
+                      <a href=""><img src="<?php echo($picture)?>" name="" width="140" height="140" class="img-circle"></a>
                    <input type="hidden" name="hidden_user_id" value="<?php echo($userRow["user_id"])?>">
                   <h4><?php echo($userRow["firstname"] . " " . $userRow["lastname"])?></h4>
                   <button type="submit" name="add_friend" value="Add Friend" class="btn btn-default">Add Friend</button>
+                      </form>
                  </center>
+
               </div>
-          </form>
             <?php
                 }
             }
